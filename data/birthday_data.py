4def9cc554bb2a9e3e07342b730f8dbf0fc51d90
Bananah-id-win <tatapapangkorn@gmail.com>
2026-09-07 08:59:33 +0700

    Update birthday flower data for multiple months

--- data/birthday_data.py
+++ data/birthday_data.py
@@ -1,8 +1,58 @@
 # data/birthday_data.py
 
 BIRTHDAY_FLOWERS = {
+    "January": {
+        "1": "The Snowdrop", "2": "The Wild Violet", "3": "The Iris", "4": "The Yellow Hyacinth",
+        "5": "The Cyclamen", "6": "The Carnation", "7": "The White Camellia", "8": "The Purple Violet",
+        "9": "The Laurel", "10": "The Box Tree", "11": "The Moss", "12": "The Calla Lily",
+        "13": "The Yellow Rose", "14": "The White Pink", "15": "The Ivy", "16": "The Red Camellia",
+        "17": "The Star of Bethlehem", "18": "The Primrose", "19": "The Golden Rod", "20": "The Dahlia",
+        "21": "The Anemone", "22": "The Speedwell", "23": "The Snowdrop (Double)", "24": "The St. John's Wort",
+        "25": "The Winter Cherry", "26": "The Mistletoe", "27": "The Lobelia", "28": "The Red Pink",
+        "29": "The Moss Rose", "30": "The Primrose (Evening)", "31": "The Christmas Rose"
+    },
+    "February": {
+        "1": "The Primrose", "2": "The Snowdrop", "3": "The Violet", "4": "The Polyanthus",
+        "5": "The Primrose (Red)", "6": "The Hyacinth", "7": "The Red Pink", "8": "The Narcissus",
+        "9": "The Hepatica", "10": "The Daphne", "11": "The Yellow Narcissus", "12": "The Crocus",
+        "13": "The Red Tulip", "14": "The Blue Violet", "15": "The Daisy", "16": "The Apricot Blossom",
+        "17": "The White Violet", "18": "The Almond", "19": "The Veronica", "20": "The Coreopsis",
+        "21": "The Speedwell", "22": "The Rosebud", "23": "The Carnation", "24": "The Peach Blossom",
+        "25": "The Peach", "26": "The Periwinkle", "27": "The White Lily", "28": "The Crocus (Yellow)",
+        "29": "The Forget-Me-Not"
+    },
+    "March": {
+        "1": "The Daffodil", "2": "The Violet", "3": "The Marigold", "4": "The Wood Sorrel",
+        "5": "The Chickweed", "6": "The Jonquil", "7": "The White Daisy", "8": "The Yellow Jonquil",
+        "9": "The Clover", "10": "The Apple Blossom", "11": "The Bluebell", "12": "The Primrose",
+        "13": "The Dandelion", "14": "The White Lilac", "15": "The Anemone", "16": "The Cowslip",
+        "17": "The Shamrock", "18": "The Sweet Pea", "19": "The Blue Violet", "20": "The Tulip",
+        "21": "The Red Tulip", "22": "The Camellia", "23": "The Pink Carnation", "24": "The Primrose (Yellow)",
+        "25": "The White Lily", "26": "The Fern", "27": "The Narcissus", "28": "The Hyacinth",
+        "29": "The Jonquil (Double)", "30": "The Sweet Briar", "31": "The White Rose"
+    },
+    "April": {
+        "1": "The Almond", "2": "The Windflower", "3": "The Daffodil", "4": "The Red Anemone",
+        "5": "The Daisy", "6": "The Sweet Pea", "7": "The Bellflower", "8": "The Broom",
+        "9": "The Hydrangea", "10": "The Stock", "11": "The Lily", "12": "The Yellow Rose",
+        "13": "The Alyssum", "14": "The White Morning Glory", "15": "The Beautiful Marigold", "16": "The Tulip",
+        "17": "The Red Poppy", "18": "The White Pink", "19": "The Pansy", "20": "The Honeysuckle",
+        "21": "The Willow", "22": "The Camphor", "23": "The Sweet Sultan", "24": "The Bluebell",
+        "25": "The Lilac", "26": "The Dogwood", "27": "The Primrose", "28": "The Japanese Primrose",
+        "29": "The Camellia", "30": "The Golden Chain"
+    },
+    "May": {
+        "1": "The Cowslip Primrose", "2": "The Buttercup", "3": "The Dandelion", "4": "The Hawthorn",
+        "5": "The Lily of the Valley", "6": "The Red Rose", "7": "The White Rose", "8": "The Yellow Rose",
+        "9": "The Violet", "10": "The Peony", "11": "The Carnation", "12": "The Iris",
+        "13": "The Forget-Me-Not", "14": "The Columbine", "15": "The Forget Me Not", "16": "The Gentian",
+        "17": "The White Lilac", "18": "The Laburnum", "19": "The Azalea", "20": "The Woodsorrel",
+        "21": "The Fuchsia", "22": "The Speedwell", "23": "The Heliotrope", "24": "The Cornflower",
+        "25": "The Marigold", "26": "The Rhododendron", "27": "The Buttercup (Yellow)", "28": "The Sweet Briar",
+        "29": "The Daisy", "30": "The Purple Lilac", "31": "The Scilla"
+    },
     "June": {
-        "1": "The Rose (Red)", "2": "The Red Columbine", "3": "The Flax", "4": "The Damask Rose", 
+        "1": "The Rose (Red)", "2": "The Red Columbine", "3": "The Flax", "4": "The Damask Rose",
         "5": "The Marigold", "6": "The Yellow Iris", "7": "The Veronica Schmidtiana", "8": "The Jasmine",
         "9": "The Sweet Pea", "10": "The Sweet Basil", "11": "The Fritillaria", "12": "The Mignonette",
         "13": "The Foxglove", "14": "The Anemone", "15": "The Carnation", "16": "The Tuberose",
@@ -11,13 +61,64 @@ BIRTHDAY_FLOWERS = {
         "25": "The Morning Glory", "26": "The White Lilac", "27": "The Passion Flower", "28": "The Geranium",
         "29": "The Red Geranium", "30": "The Honey Suckle"
     },
-    "April": {
-        "1": "The Almond", "2": "The Windflower", "3": "The Daffodil", "4": "The Red Anemone",
-        "14": "The White Morning Glory", "15": "The Beautiful Marigold", "16": "The Tulip", 
-        "28": "The Japanese Primrose", "29": "The Camellia", "30": "The Golden Chain"
+    "July": {
+        "1": "The Larkspur", "2": "The Water Lily", "3": "The Poppy", "4": "The White Rose",
+        "5": "The Sweet Pea", "6": "The Orchid", "7": "The Water Dock", "8": "The Red Pink",
+        "9": "The Fuchsia", "10": "The Snapdragon", "11": "The White Pink", "12": "The Zinnia",
+        "13": "The Hydrangea", "14": "The Verbena", "15": "The Camellias", "16": "The Nasturtium",
+        "17": "The White Rose", "18": "The Clematis", "19": "The Hollyhock", "20": "The Carnation",
+        "21": "The Morning Glory", "22": "The Water Lily (Yellow)", "23": "The Phlox", "24": "The Yellow Rose",
+        "25": "The Sunflower", "26": "The Chamomile", "27": "The Geranium", "28": "The Sweet Sultan",
+        "29": "The Marigold", "30": "The Oleander", "31": "The White Jasmine"
     },
-    "May": {
-        "1": "The Cowslip Primrose", "2": "The Buttercup", "3": "The Dandelion", "5": "The Lily of the Valley",
-        "15": "The Forget Me Not", "20": "The Woodsorrel", "30": "The Purple Lilac", "31": "The Scilla"
+    "August": {
+        "1": "The Gladiolus", "2": "The Poppy", "3": "The Hollyhock", "4": "The Dahlia",
+        "5": "The Sunflower", "6": "The Aster", "7": "The Golden Rod", "8": "The Morning Glory",
+        "9": "The Oleander", "10": "The Hydrangea", "11": "The Passion Flower", "12": "The Red Poppy",
+        "13": "The Zinnia", "14": "The White Rose", "15": "The Marigold", "16": "The Petunia",
+        "17": "The Clematis", "18": "The Heliotrope", "19": "The Coreopsis", "20": "The Cornflower",
+        "21": "The Water Lily", "22": "The Larkspur", "23": "The Orange Blossom", "24": "The Speedwell",
+        "25": "The White Jasmine", "26": "The Nasturtium", "27": "The Phlox", "28": "The Verbena",
+        "29": "The Lotus", "30": "The Tiger Lily", "31": "The Camellia"
+    },
+    "September": {
+        "1": "The Aster", "2": "The Morning Glory", "3": "The Dahlia", "4": "The Marigold",
+        "5": "The Golden Rod", "6": "The Fuchsia", "7": "The Water Lily", "8": "The Hydrangea",
+        "9": "The Zinnia", "10": "The White Jasmine", "11": "The Passion Flower", "12": "The Clematis",
+        "13": "The Gladiolus", "14": "The Bluebell", "15": "The Sunflower", "16": "The Oleander",
+        "17": "The Chrysanthemum", "18": "The Tiger Lily", "19": "The Nasturtium", "20": "The Clover",
+        "21": "The Poppy", "22": "The Petunia", "23": "The Camellia", "24": "The Red Rose",
+        "25": "The White Rose", "26": "The Carnation", "27": "The Coreopsis", "28": "The Heliotrope",
+        "29": "The Cosmos", "30": "The Sweet Pea"
+    },
+    "October": {
+        "1": "The Marigold", "2": "The Cosmos", "3": "The Chrysanthemum", "4": "The Dahlia",
+        "5": "The Golden Rod", "6": "The Aster", "7": "The Hydrangea", "8": "The Camellia",
+        "9": "The White Rose", "10": "The Red Rose", "11": "The Zinnia", "12": "The Clematis",
+        "13": "The Geranium", "14": "The Morning Glory", "15": "The Passion Flower", "16": "The Oleander",
+        "17": "The Carnation", "18": "The Hollyhock", "19": "The Nasturtium", "20": "The Sunflower",
+        "21": "The Coreopsis", "22": "The Petunia", "23": "The Violet", "24": "The Lily",
+        "25": "The Orchid", "26": "The Bluebell", "27": "The Verbena", "28": "The Poppy",
+        "29": "The Daisy", "30": "The Sweet Pea", "31": "The Calendula"
+    },
+    "November": {
+        "1": "The Chrysanthemum", "2": "The White Chrysanthemum", "3": "The Yellow Chrysanthemum", "4": "The Red Chrysanthemum",
+        "5": "The Dahlia", "6": "The Camellia", "7": "The Hydrangea", "8": "The Golden Rod",
+        "9": "The Marigold", "10": "The Aster", "11": "The Zinnia", "12": "The Verbena",
+        "13": "The Nasturtium", "14": "The Morning Glory", "15": "The Passion Flower", "16": "The Oleander",
+        "17": "The Carnation", "18": "The Hollyhock", "19": "The Sunflower", "20": "The Geranium",
+        "21": "The Clematis", "22": "The Orchid", "23": "The Violet", "24": "The Lily",
+        "25": "The Rose", "26": "The Daisy", "27": "The Poppy", "28": "The Sweet Pea",
+        "29": "The Iris", "30": "The Primrose"
+    },
+    "December": {
+        "1": "The Poinsettia", "2": "The Holly", "3": "The Mistletoe", "4": "The White Rose",
+        "5": "The Ivy", "6": "The Narcissus", "7": "The Paperwhite", "8": "The Amaryllis",
+        "9": "The Camellia", "10": "The Cyclamen", "11": "The Christmas Rose", "12": "The Snowdrop",
+        "13": "The Chrysanthemum", "14": "The Aster", "15": "The Marigold", "16": "The Golden Rod",
+        "17": "The Hydrangea", "18": "The Carnation", "19": "The Violet", "20": "The Iris",
+        "21": "The Lily", "22": "The Orchid", "23": "The Dahlia", "24": "The Fir Tree",
+        "25": "The Holly (Red)", "26": "The Mistletoe (White)", "27": "The Poinsettia (Red)", "28": "The Rose",
+        "29": "The Daisy", "30": "The Primrose", "31": "The White Camellia"
     }
 }
